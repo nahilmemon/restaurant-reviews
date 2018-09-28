1827364b97a05ce84e85cc12d249f6e52119abf3
Nahil Memon <18367304+nahilmemon@users.noreply.github.com>
2018-09-28 16:46:36 +0400

    feat: Register a service worker.

--- js/main.js
+++ js/main.js
@@ -224,3 +224,21 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 } */
 
+/**
+ * Register a service worker.
+ * Code taken from https://developers.google.com/web/fundamentals/primers/service-workers/
+ */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
